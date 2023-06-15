3bf7202ad2d6622a3fabf9a795340491b5e24730
Benedikt Brauneck <b.brauneck@ris.ag>
2023-06-15 14:45:32 +0200

    feat: enabled labelling module

--- variables.tf
+++ variables.tf
@@ -132,3 +132,13 @@ locals {
     }
   ]
 }
+variable "context" {
+  type = object({
+    organization = string
+    environment  = string
+    account      = string
+    product      = string
+    tags         = map(string)
+  })
+  description = "Default context for naming and tagging purpose"
+}
